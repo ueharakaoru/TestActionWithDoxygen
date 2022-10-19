fb4bf7acff0c691e0ab7e14b779f1f735c282da2
ueharakaoru <k.ueharakaoru@gmail.com>
2022-10-19 16:55:41 +0900

    add files

---
+++ a.hpp
@@ -0,0 +1,18 @@
+/// @file
+/// \brief ソースファイル
+
+#include <iostream>
+
+/// @brief 名前空間
+namespace test_action {
+
+/// @brief クラス Hoge
+class Hoge {
+
+    /// @brief 関数func
+    void func() {
+        std::cout << "Hoge" << std::endl;
+    }
+};
+
+}
---
+++ mainpage.md
@@ -0,0 +1,3 @@
+\mainpage GitHubActionでDoxygen
+
+GitHubActionでDoxygenをビルドしたい．
